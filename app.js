ad23b0270805ec8c9b9f345235c82b9a96cdee90
BSenichak <bsenk77@gmail.com>
2026-09-23 19:18:44 +0300

    asdasd

--- app.js
+++ app.js
@@ -2,16 +2,24 @@ const express = require("express")
 
 const app = express()
 
+app.set("view engine", "ejs")
+app.set("views", "views")
+
 app.use(express.static("static"))
 app.use(express.json())
 
 let ads = []
+
+app.get("/", (req, res)=>{
+    res.render("index")
+})
 
 app.post("/add", (req, res)=>{
     ads.push(req.body)
     res.status(200)
     res.send()
 })
+
 
 app.get("/ads", (req, res)=> {
     res.status(200)
